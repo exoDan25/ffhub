0872f6c95c68a9be83d9193b7f281ccfee130b4f
exoDan25 <dan@exoreach.co>
2025-08-31 10:47:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ wss.on("connection", (twilioWS, req) => {
 
   // Connect to ElevenLabs Convai Agent
   const elevenlabsWS = new WebSocket(
-  `wss://api.elevenlabs.io/v1/convai/agent/${process.env.ELEVENLABS_AGENT_ID}/stream?model=eleven_monolingual_v1`,
+  `wss://api.elevenlabs.io/v1/convai/agent/${process.env.ELEVENLABS_AGENT_ID}/stream`,
   {
     headers: { "xi-api-key": process.env.ELEVENLABS_API_KEY },
   }
